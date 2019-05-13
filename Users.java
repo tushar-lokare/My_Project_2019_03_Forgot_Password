f55ddab8d3cbb66557332f12377b1e8063079a0e
Tushar Lokare <contacttusharlokare@gmail.com>
2019-05-13 21:59:06 +0530

    pojo class for 4 variables
    id, name, userName, pass

---
+++ Users.java
@@ -0,0 +1,40 @@
+package com.Pojo;
+
+public class Users {
+
+	private int id;
+	private String name, userName, pass;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getPass() {
+		return pass;
+	}
+
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+
+}
